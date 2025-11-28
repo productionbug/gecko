384f29ad3dcd0fcd20bec72ce9fb5a4d159faeaf
Wai Yan Phyo <oyhpnayiaw@gmail.com>
2025-11-28 11:41:32 +0700

    Add typescript module argumentation examples

---
+++ apps/docs/gecko.d.ts
@@ -0,0 +1,16 @@
+import "@productionbug/gecko";
+
+declare module "@productionbug/gecko" {
+  interface ButtonColorMap {
+    secondary: unknown;
+    danger: unknown;
+  }
+
+  interface SwitchSizeMap {
+    lg: unknown;
+  }
+
+  interface AlertVariantMap {
+    critical: unknown;
+  }
+}
